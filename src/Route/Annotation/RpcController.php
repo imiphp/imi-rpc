9cb338c96b9c85d864c4b7ab2dea02df19ab68cb
Yurun <admin@yurunsoft.com>
2021-05-17 13:26:25 +0800

    新增支持在控制器注解中，指定当前控制器允许哪些服务器使用
    doc/components/httpserver/route.md                         |  1 +
    doc/components/rpc/hprose.md                               |  4 +++-
    doc/components/tcpServer/controller.md                     | 11 +++++++++++
    doc/components/udpServer/controller.md                     | 11 +++++++++++
    doc/components/websocketServer/websocketController.md      | 10 ++++++++++
    src/Components/rpc/src/Listener/RouteInit.php              |  4 ++++
    src/Components/rpc/src/Route/Annotation/RpcController.php  |  9 +++++++++
    src/Server/Route/Annotation/Controller.php                 |  9 +++++++++
    src/Server/Route/Annotation/Tcp/TcpController.php          |  9 +++++++++
    src/Server/Route/Annotation/Udp/UdpController.php          |  9 +++++++++
    src/Server/Route/Annotation/WebSocket/WSController.php     |  9 +++++++++
    src/Server/Route/Listener/HttpRouteInit.php                |  4 ++++
    src/Server/Route/Listener/TcpRouteInit.php                 |  4 ++++
    src/Server/Route/Listener/UdpRouteInit.php                 |  4 ++++
    src/Server/Route/Listener/WSRouteInit.php                  |  4 ++++
    tests/unit/HttpServer/HttpsTestServer/config/config.php    |  1 +
    tests/unit/HttpServer/OutsideController/TestController.php |  2 +-
    tests/unit/HttpServer/Tests/HttpsTest.php                  | 13 +++++++++++++
    18 files changed, 116 insertions(+), 2 deletions(-)

--- src/Route/Annotation/RpcController.php
+++ src/Route/Annotation/RpcController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Imi\Rpc\Route\Annotation;
 
 use Imi\Bean\Annotation\Base;
@@ -6,7 +7,8 @@ use Imi\Bean\Annotation\Parser;
 use Imi\Rpc\Route\Annotation\Contract\IRpcController;
 
 /**
- * RPC 控制器注解
+ * RPC 控制器注解.
+ *
  * @Annotation
  * @Target("CLASS")
  * @Parser("Imi\Rpc\Route\Annotation\Parser\RpcControllerParser")
@@ -14,14 +16,25 @@ use Imi\Rpc\Route\Annotation\Contract\IRpcController;
 class RpcController extends Base implements IRpcController
 {
     /**
-     * 只传一个参数时的参数名
+     * 只传一个参数时的参数名.
+     *
      * @var string
      */
     protected $defaultFieldName = 'prefix';
 
     /**
      * 路由前缀
+     *
      * @var string
      */
     public $prefix;
+
+    /**
+     * 指定当前控制器允许哪些服务器使用.
+     *
+     * 支持字符串或数组，默认为 null 则不限制
+     *
+     * @var string|string[]|null
+     */
+    public $server = null;
 }
